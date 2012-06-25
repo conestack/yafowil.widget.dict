2fe1996388f9061c9baa6f5ddfc5df1d0a3c8884
Robert Niederreiter <office@squarewave.at>
2012-06-25 17:21:46 +0200

    resources

--- src/yafowil/widget/dict/__init__.py
+++ src/yafowil/widget/dict/__init__.py
@@ -1,25 +1,39 @@
 import os
+from yafowil.base import factory
+
+
+resourcedir = os.path.join(os.path.dirname(__file__), 'resources')
+
+js = [{
+    'resource': 'widget.js',
+    'thirdparty': False,
+    'order': 20,
+}]
+
+css = [{
+    'resource': 'widget.css',
+    'thirdparty': False,
+    'order': 20,
+}]
 
 
 def register():
     import widget
+    factory.register_theme('default', 'yafowil.widget.dict',
+                           resourcedir, js=js, css=css)
 
+
+###############################################################################
+# XXX: outdated below
+###############################################################################
 
 def get_resource_dir():
-    return os.path.join(os.path.dirname(__file__), 'resources')
+    return resourcedir
 
 
 def get_js():
-    return [{
-        'resource': 'widget.js',
-        'thirdparty': False,
-        'order': 20,
-    }]
+    return js
 
 
 def get_css():
-    return [{
-        'resource': 'widget.css',
-        'thirdparty': False,
-        'order': 20,
-    }]
+    return css
