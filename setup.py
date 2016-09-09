d06850173c7be5dc5b25ad092a4d20952425dfbf
Jens W. Klein <jk@kleinundpartner.at>
2016-09-09 17:33:45 +0200

    house keeping

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ import os
 version = '1.6.dev0'
 shortdesc = 'Dict/Mapping Widget for YAFOWIL'
 longdesc = open(os.path.join(os.path.dirname(__file__), 'README.rst')).read()
-longdesc += open(os.path.join(os.path.dirname(__file__), 'HISTORY.rst')).read()
+longdesc += open(os.path.join(os.path.dirname(__file__), 'CHANGES.rst')).read()
 longdesc += open(os.path.join(os.path.dirname(__file__), 'LICENSE.rst')).read()
 tests_require = ['yafowil[test]']
 
@@ -20,16 +20,17 @@ setup(
         'Environment :: Web Environment',
         'Operating System :: OS Independent',
         'Programming Language :: Python',
+        'Programming Language :: Python :: 2.7',
         'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
         'License :: OSI Approved :: BSD License',
     ],
-    keywords='',
+    keywords='yafowil form widget dict mapping',
     author='BlueDynamics Alliance',
     author_email='dev@bluedynamics.com',
     url=u'http://pypi.python.org/pypi/yafowil.widget.dict',
     license='Simplified BSD',
     packages=find_packages('src'),
-    package_dir = {'': 'src'},
+    package_dir={'': 'src'},
     namespace_packages=['yafowil', 'yafowil.widget'],
     include_package_data=True,
     zip_safe=False,
@@ -38,7 +39,7 @@ setup(
         'yafowil>2.1.99',
     ],
     tests_require=tests_require,
-    extras_require = dict(
+    extras_require=dict(
         test=tests_require,
     ),
     test_suite="yafowil.widget.dict.tests.test_suite",
