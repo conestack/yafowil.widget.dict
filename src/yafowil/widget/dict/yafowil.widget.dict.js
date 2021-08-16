634f991bdbc6ed022d4c759ab54e5e04ae2d3ebe
Lena Daxenbichler <lena.daxenbichler@r2solutions.at>
2021-08-16 11:10:42 +0200

    setup karma and rollup

---
+++ src/yafowil/widget/dict/yafowil.widget.dict.js
@@ -0,0 +1,15 @@
+(function () {
+	'use strict';
+
+
+
+
+	if (window.yafowil === undefined) {
+	    window.yafowil = {};
+	}
+
+	window.yafowil.dict = exports;
+
+
+}());
+//# sourceMappingURL=yafowil.widget.dict.js.map
